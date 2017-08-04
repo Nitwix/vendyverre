397b2313fc5a9fb087f826f1f48d7a817bde29a3
Nina Ionescu <ninachaton@gmail.com>
2017-08-04 21:56:35 +0200

    changement mineur dans weather

--- src/scripts/Weather.js
+++ src/scripts/Weather.js
@@ -1,6 +1,6 @@
 function addSnow(){
 	snowEmitter = game.add.emitter(game.world.centerX,-30,500);
-	snowEmitter.makeParticles("snowflakes",[0,1,2,3,4,5,6,7,8]);
+	ssnowEmitter.makeParticles("snowflakes",[0,1,2,3,4,5,6,7,8]);
 	snowEmitter.maxParticleScale = 1;
 	snowEmitter.minParticleScale = 0.3;
 	snowEmitter.setYSpeed(20, 100);
